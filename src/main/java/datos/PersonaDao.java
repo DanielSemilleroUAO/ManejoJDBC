a0843e730e7323919813979806b972eb9e42045e
Daniel Delgado Rodríguez <danieldelgado_2106@hotmail.com>
2022-04-13 21:56:21 -0500

    Se agrega delete y update persona

--- src/main/java/datos/PersonaDao.java
+++ src/main/java/datos/PersonaDao.java
@@ -19,6 +19,9 @@ import java.util.logging.Logger;
 public class PersonaDao {
 
     private static final String SQL_SELECT = "SELECT * FROM persona";
+    private static final String SQL_INSERT = "INSERT INTO persona(nombre, apellido, email, telefono) VALUES(?,?,?,?)";
+    private static final String SQL_UPDATE = "UPDATE persona SET nombre = ?, apellido = ?, email = ?, telefono = ? WHERE id_persona = ?";
+    private static final String SQL_DELETE = "DELETE FROM persona WHERE id_persona = ?";
 
     public List<Persona> seleccionar() {
         Connection conn = null;
@@ -56,6 +59,84 @@ public class PersonaDao {
         }
 
         return personas;
+    }
+
+    public int insertar(Persona persona) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        int registros = 0;
+        try {
+            conn = getConnection();
+            stmt = conn.prepareStatement(SQL_INSERT);
+            stmt.setString(1, persona.getName());
+            stmt.setString(2, persona.getApellido());
+            stmt.setString(3, persona.getEmail());
+            stmt.setString(4, persona.getTelefono());
+            registros = stmt.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        } finally {
+            try {
+                close(stmt);
+                close(conn);
+            } catch (SQLException ex) {
+                ex.printStackTrace(System.out);
+            }
+        }
+
+        return registros;
 
     }
+
+    public int actualizar(Persona persona) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        int registros = 0;
+        try {
+            conn = getConnection();
+            stmt = conn.prepareStatement(SQL_UPDATE);
+            stmt.setString(1, persona.getName());
+            stmt.setString(2, persona.getApellido());
+            stmt.setString(3, persona.getEmail());
+            stmt.setString(4, persona.getTelefono());
+            stmt.setInt(5, persona.getIdPersona());
+            registros = stmt.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        } finally {
+            try {
+                close(stmt);
+                close(conn);
+            } catch (SQLException ex) {
+                ex.printStackTrace(System.out);
+            }
+        }
+
+        return registros;
+
+    }
+    
+    public int eliminar(Persona persona) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        int registros = 0;
+        try {
+            conn = getConnection();
+            stmt = conn.prepareStatement(SQL_DELETE);
+            stmt.setInt(1, persona.getIdPersona());
+            registros = stmt.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        } finally {
+            try {
+                close(stmt);
+                close(conn);
+            } catch (SQLException ex) {
+                ex.printStackTrace(System.out);
+            }
+        }
+        return registros;
+
+    }
+    
 }
--- src/main/java/test/TestManejoPersonas.java
+++ src/main/java/test/TestManejoPersonas.java
@@ -14,9 +14,23 @@ import java.util.List;
  * @author adseglocdom
  */
 public class TestManejoPersonas {
+
     public static void main(String[] args) {
         PersonaDao personaDao = new PersonaDao();
-        List<Persona> personas =  personaDao.seleccionar();
+        List<Persona> personas = personaDao.seleccionar();
         personas.forEach(persona -> System.out.println(persona.toString()));
+
+        Persona personaNueva = new Persona("Daniel", "Delgado", "daniel@gmail.com", "11223344");
+        personaDao.insertar(personaNueva);
+        
+        Persona personaModificar = new Persona(1, "katerin", "Delgado", "katerin@gmail.com", "11223344");
+        personaDao.actualizar(personaModificar);
+
+        Persona personaEliminar = new Persona(1);
+        personaDao.eliminar(personaEliminar);
+
+        personas = personaDao.seleccionar();
+        personas.forEach(persona -> System.out.println(persona.toString()));
+
     }
 }
